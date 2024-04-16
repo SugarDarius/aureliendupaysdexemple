3fa33ce53aded246d9a806d1af49b883d46bffb8
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-16 11:14:15 +0200

    refactor: use good types for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,10 @@
+import { MetadataRoute } from 'next'
+
 export const baseUrl = 'https://aureliendupaysdexemple.com/'
 
 const PATHNAMES = ['/']
 
-type SitemapItem = { url: string; lastModified: string }
-
-export default async function sitemap(): Promise<SitemapItem[]> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const routes = PATHNAMES.map((pathname) => ({
     url: `${baseUrl}${pathname}`,
     lastModified: new Date().toISOString().split('T')[0],
