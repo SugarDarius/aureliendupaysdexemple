ce6a8f68748330f30265da4ace195ad30bcdf305
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-16 11:32:18 +0200

    chore: add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <div className='flex h-screen w-screen flex-col items-center justify-center'>
+      <h1 className='text-2xl font-semibold tracking-tighter'>
+        404 - Page Not Found
+      </h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  )
+}
