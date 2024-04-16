e3a355eedcfcf6ebf846d7024eb9204d5c323d51
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-16 17:24:25 +0200

    chore: add site config and metadata

---
+++ config/site-config.ts
@@ -0,0 +1,24 @@
+import { baseUrl } from '@/app/sitemap'
+export const siteConfig = {
+  title: 'Aurélien Dupays Dexemple',
+  description:
+    "Hello, I'm Aurélien Dupays Dexemple and this is my personal website.",
+  socialLinks: {
+    twitter: {
+      url: 'https://twitter.com/azeldvin',
+      name: '@azeldvin',
+    },
+    github: {
+      url: 'https://github.com/SugarDarius',
+      name: 'SugarDarius',
+    },
+  },
+  authors: [
+    {
+      name: 'SugarDarius',
+      url: baseUrl,
+    },
+  ],
+  creator: 'SugarDarius',
+}
+export type SiteConfig = typeof siteConfig
