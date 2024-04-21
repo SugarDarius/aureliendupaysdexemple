b13bc7a01bd5c0daf545aadae7e0c3821db9a9c5
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-21 12:40:00 +0200

    chore: add social links

---
+++ components/icons/twitter-logo.tsx
@@ -0,0 +1,15 @@
+export const TwitterLogoIcon = (props: React.HTMLAttributes<SVGElement>) => (
+  <svg
+    {...props}
+    width='22'
+    height='20'
+    viewBox='0 0 22 20'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M17.2439 0.25H20.5519L13.3249 8.51L21.8269 19.75H15.1699L9.95591 12.933L3.98991 19.75H0.679906L8.40991 10.915L0.253906 0.25H7.07991L11.7929 6.481L17.2439 0.25ZM16.0829 17.77H17.9159L6.08391 2.126H4.11691L16.0829 17.77Z'
+      fill='currentColor'
+    />
+  </svg>
+)
--- components/navigation/navigation-dock.tsx
+++ components/navigation/navigation-dock.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { forwardRef } from 'react'
 
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
+
+import { siteConfig } from '@/config/site-config'
 import { cn } from '@/lib/utils'
 import {
   type NavigationItemName,
@@ -11,6 +14,7 @@ import {
   navigationItemsIcons,
 } from '@/lib/navigation'
 
+import { TwitterLogoIcon } from '@/components/icons/twitter-logo'
 import {
   Tooltip,
   TooltipTrigger,
@@ -23,6 +27,7 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
+import { Separator } from '@/components/ui/separator'
 
 type NavigationDockItemLinkProps = {
   href: string
@@ -49,6 +54,28 @@ const NavigationDockItemLink = forwardRef<
   )
 })
 NavigationDockItemLink.displayName = 'NavigationDockItemLink'
+
+const NavigationDockItemExternalLink = ({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) => {
+  return (
+    <a
+      href={href}
+      target='_blank'
+      rel='noopener noreferrer'
+      className={cn(
+        navigationMenuTriggerStyle(),
+        'flex h-9 w-9 flex-col items-center justify-center rounded-full p-0 data-[active]:bg-accent'
+      )}
+    >
+      {children}
+    </a>
+  )
+}
 
 const NavigationDockItem = ({
   href,
@@ -84,18 +111,33 @@ export function NavigationDock() {
   const pathname = usePathname()
 
   return (
-    <div className='fixed bottom-6 left-0 right-0 mx-auto my-0 w-max rounded-xl border-2 p-2'>
-      <NavigationMenu>
-        <NavigationMenuList>
-          {navigationItems.map((navigationItem) => (
-            <NavigationDockItem
-              key={navigationItem.name}
-              {...navigationItem}
-              pathname={pathname}
-            />
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div className='fixed bottom-6 left-0 right-0 mx-auto my-0 w-max'>
+      <div className='relative flex flex-row items-center gap-2 rounded-xl border-2 p-2'>
+        <NavigationMenu>
+          <NavigationMenuList>
+            {navigationItems.map((navigationItem) => (
+              <NavigationDockItem
+                key={navigationItem.name}
+                {...navigationItem}
+                pathname={pathname}
+              />
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Separator orientation='vertical' className='h-[26px]' />
+        <div className='flex flex-row items-center gap-1'>
+          <NavigationDockItemExternalLink
+            href={siteConfig.socialLinks.github.url}
+          >
+            <GitHubLogoIcon className='h-4 w-4' />
+          </NavigationDockItemExternalLink>
+          <NavigationDockItemExternalLink
+            href={siteConfig.socialLinks.twitter.url}
+          >
+            <TwitterLogoIcon className='h-4 w-4' />
+          </NavigationDockItemExternalLink>
+        </div>
+      </div>
     </div>
   )
 }
