4fac4fdfb312d529ad5193e8c59fa0a1ecca359b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-22 14:12:09 +0200

    refactor: update not found text

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
 export default function NotFoundPage() {
   return (
-    <div className='flex h-full w-full flex-col items-center justify-center'>
-      <h1 className='text-2xl font-semibold tracking-tighter'>
-        404 - Page Not Found
-      </h1>
+    <div className='flex h-full w-full flex-col items-center justify-center gap-0.5'>
+      <div className='text-8xl font-extrabold tracking-tighter'>
+        <h1 className='bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>
+          404
+        </h1>
+      </div>
       <p>The page you are looking for does not exist.</p>
     </div>
   )
