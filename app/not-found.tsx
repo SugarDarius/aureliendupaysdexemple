d208fcdce9a51f41b9d333651d857193ca8ab0da
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-22 14:36:23 +0200

    refactor: import particles in not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,19 @@
+import { ParticlesScene } from '@/components/particles/particles-scene'
+
 export default function NotFoundPage() {
   return (
-    <div className='flex h-full w-full flex-col items-center justify-center gap-0.5'>
-      <div className='text-8xl font-extrabold tracking-tighter'>
-        <h1 className='bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>
-          404
-        </h1>
+    <div className='relative flex h-full w-full flex-col items-center justify-center gap-0.5'>
+      <div className='absolute left-0 top-0 z-0 h-full w-full overflow-hidden'>
+        <ParticlesScene />
       </div>
-      <p>The page you are looking for does not exist.</p>
+      <div className='z-10 flex flex-col items-center'>
+        <div className='text-8xl font-extrabold tracking-tighter'>
+          <h1 className='bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>
+            404
+          </h1>
+        </div>
+        <p>The page you are looking for does not exist.</p>
+      </div>
     </div>
   )
 }
--- components/particles/particles-scene.tsx
+++ components/particles/particles-scene.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
-export function Scene() {
+export function ParticlesScene() {
   return (
     <Canvas camera={{ position: [1.5, 1.5, 1.5] }}>
       <ambientLight intensity={0.5} />
+      <OrbitControls />
     </Canvas>
   )
 }
