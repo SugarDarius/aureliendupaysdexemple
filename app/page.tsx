24e24a5c7ec6b6d4d50b4452b6e65b670292afee
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-23 16:03:20 +0200

    refactor: update logo size

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function HomePage() {
   return (
     <div className='relative flex h-full w-full flex-col items-center justify-center'>
       <BentoGrid>
-        <BentoCard className='col-span-1'>
+        <BentoCard className='col-span-1 row-span-1'>
           <div className='flex h-full w-full flex-col items-center justify-center gap-2 p-4'>
             <Image
               src='/aureliendupaysdexemple-logo.png'
-              width={200}
-              height={200}
+              width={160}
+              height={160}
               alt='logo'
             />
           </div>
