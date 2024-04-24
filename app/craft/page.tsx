98066d0153119aa0ecf67e04bb71eaf7b8ec593b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-24 15:03:39 +0200

    chore: add coming soon page content

---
+++ app/craft/page.tsx
@@ -0,0 +1,9 @@
+import { ComingSoonPageContent } from '@/components/misc/coming-soon-page-content'
+
+export default function CraftPage() {
+  return (
+    <div className='relative flex h-full w-full flex-col'>
+      <ComingSoonPageContent />
+    </div>
+  )
+}
---
+++ components/misc/coming-soon-page-content.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { ParticlesPage } from '@/components/particles/particles-page'
+
+export function ComingSoonPageContent() {
+  return (
+    <ParticlesPage title='Coming soon'>
+      <p className='text-xl font-bold tracking-tight text-muted-foreground'>
+        This page will be live soon.
+      </p>
+    </ParticlesPage>
+  )
+}
