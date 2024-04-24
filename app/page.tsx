37a7e4ca4a4fb332ddbd58856083996d8ce8733f
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-24 17:48:38 +0200

    chore: add linkedin bento item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { BentoGrid } from '@/components/grids/bento-grid'
 import { GitHubBentoItem } from '@/components/bento/github-bento-item'
 import { HeroBentoItem } from '@/components/bento/hero-bento-item'
 import { TwitterBentoItem } from '@/components/bento/twitter-bento-item'
+import { LinkedInBentoItem } from '@/components/bento/linkedin-bento-item'
 
 export default function HomePage() {
   return (
@@ -11,6 +12,7 @@ export default function HomePage() {
         <HeroBentoItem />
         <GitHubBentoItem />
         <TwitterBentoItem />
+        <LinkedInBentoItem />
       </BentoGrid>
     </div>
   )
--- components/bento/linkedin-bento-item.tsx
+++ components/bento/linkedin-bento-item.tsx
@@ -2,21 +2,21 @@ import Image from 'next/image'
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 
 import { siteConfig } from '@/config/site-config'
-import { TwitterSquareIcon } from '@/components/icons/twitter-square-icon'
+import { LinkedInSquareIcon } from '@/components/icons/linkedin-square-icon'
 import { BentoCard } from '@/components/grids/bento-card'
 
-export function TwitterBentoItem() {
+export function LinkedInBentoItem() {
   return (
     <BentoCard className='col-span-1 row-span-1'>
       <a
-        href={siteConfig.socialLinks.twitter.url}
+        href={siteConfig.socialLinks.linkedin.url}
         target='_blank'
         rel='noopener noreferrer'
       >
         <div className='relative flex aspect-square size-full flex-col'>
           <div className='absolute left-0 top-0 h-full w-full opacity-60'>
             <Image
-              src='/tokyo.jpg'
+              src='/forest.jpg'
               fill
               alt='logo'
               style={{
@@ -29,17 +29,17 @@ export function TwitterBentoItem() {
               sizes='720px'
             />
           </div>
-          <div className='flex size-full flex-col justify-between bg-gradient-to-b from-sky-200/75 to-sky-500/75 p-4 text-background backdrop-blur-sm dark:text-foreground'>
+          <div className='flex size-full flex-col justify-between bg-gradient-to-b from-blue-200/75 to-blue-500/75 p-4 text-background backdrop-blur-sm dark:text-foreground'>
             <div className='flex flex-row items-center justify-between'>
               <h1 className='text-xl font-extrabold tracking-tighter'>
-                Twitter (X)
+                LinkedIn
               </h1>
-              <TwitterSquareIcon className='size-6' />
+              <LinkedInSquareIcon className='size-6' />
             </div>
             <div className='flex flex-col'>
-              <h1 className='text-md font-bold tracking-tighter'>Azeldvin</h1>
+              <h1 className='text-md font-bold tracking-tighter'>Aurélien</h1>
               <div className='flex flex-row items-center gap-1'>
-                <p className='text-xs underline'>Check my Twitter profile</p>
+                <p className='text-xs underline'>Check my LinkedIn profile</p>
                 <ArrowTopRightOnSquareIcon className='inline-flex h-3.5 w-3.5' />
               </div>
             </div>
