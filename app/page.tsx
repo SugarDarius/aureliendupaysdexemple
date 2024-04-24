99dbd6d28161275aed6594d2be25074df3a65bf9
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-24 15:34:23 +0200

    refactor: invert bento items

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function HomePage() {
   return (
     <div className='relative flex h-full w-full flex-col items-center justify-center'>
       <BentoGrid>
-        <GitHubBentoItem />
         <HeroBentoItem />
+        <GitHubBentoItem />
       </BentoGrid>
     </div>
   )
