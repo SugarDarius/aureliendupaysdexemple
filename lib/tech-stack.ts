adbb6b9d7c13d18d2453ec8b59d1b172bb28bcb8
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-26 16:35:28 +0200

    chore: graphql tech stack item

--- lib/tech-stack.ts
+++ lib/tech-stack.ts
@@ -1,6 +1,7 @@
 import { HTML5Icon } from '@/components/icons/html5-icon'
 import { CSS3Icon } from '@/components/icons/css3-icon'
 import { TypescriptIcon } from '@/components/icons/typescript-icon'
+import { GraphQLIcon } from '@/components/icons/graphql-icon'
 
 type TechStackItem = {
   name: string
@@ -14,4 +15,5 @@ export const languagesTechStackItems: TechStackItem[] = [
   { name: 'HTML 5', Icon: HTML5Icon, tag: 'frontend' },
   { name: 'CSS 3', Icon: CSS3Icon, tag: 'frontend' },
   { name: 'Typescript', Icon: TypescriptIcon, tag: 'frontend/backend' },
+  { name: 'GraphQL', Icon: GraphQLIcon, tag: 'frontend/backend' },
 ]
