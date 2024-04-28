6bbcf17943072dff3c56b96e282e0104b7f091ed
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-28 10:35:56 +0200

    refactor: update padding

--- components/content/hero.tsx
+++ components/content/hero.tsx
@@ -7,7 +7,7 @@ export function Hero({
 }) {
   return (
     <div className='flex w-full flex-col gap-1'>
-      <h1 className='bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-6xl font-extrabold tracking-tighter text-transparent'>
+      <h1 className='bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-6xl font-extrabold tracking-tighter text-transparent'>
         {title}
       </h1>
       <p className='text-muted-foreground'>{description}</p>
