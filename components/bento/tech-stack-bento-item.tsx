2d58de87320b25da239f5ba54a2fd33948340504
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-28 18:33:17 +0200

    refactor: add tech stack preview

--- components/bento/tech-stack-bento-item.tsx
+++ components/bento/tech-stack-bento-item.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { PageBentoItem } from '@/components/bento/page-bento-item'
 
 export function TechStackBentoItem() {
@@ -6,6 +7,19 @@ export function TechStackBentoItem() {
       title='Tech Stack'
       description='Check my tech stack I use everyday'
       href='/tech-stack'
-    ></PageBentoItem>
+    >
+      <div className='flex h-full w-full flex-col items-center justify-center'>
+        <div className='relative flex size-[80%] flex-col'>
+          <Image
+            src='/tech-stack-preview.png'
+            fill
+            alt='logo'
+            priority
+            style={{ objectFit: 'contain' }}
+            sizes='110px'
+          />
+        </div>
+      </div>
+    </PageBentoItem>
   )
 }
