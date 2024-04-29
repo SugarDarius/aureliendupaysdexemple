f78a68e2277faf29428e0b555f428233c53a64ea
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-29 15:14:43 +0200

    refactor: scaffold experience page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
-import { ComingSoonPageContent } from '@/components/misc/coming-soon-page-content'
+
+import { Separator, Separator } from '@/components/ui/separator'
+import { Hero } from '@/components/content/hero'
 
 export const metadata: Metadata = {
   title: 'Experience',
@@ -8,8 +10,14 @@ export const metadata: Metadata = {
 
 export default function ExperiencePage() {
   return (
-    <div className='relative flex h-full w-full flex-col'>
-      <ComingSoonPageContent />
+    <div className='relative flex h-full w-full flex-col items-center'>
+      <div className='flex h-full w-full max-w-4xl flex-col gap-10 px-12 pb-24 pt-12 max-sm:px-4 min-[1025px]:px-0'>
+        <Hero
+          title='Experience'
+          description='All look at my professional experiences.'
+        />
+        <Separator />
+      </div>
     </div>
   )
 }
