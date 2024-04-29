5ed0b7b3caa5de27c4ddb7d9347fefc99c2aa8ce
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-04-29 17:33:00 +0200

    refactor: add tagline in experience page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next'
 
+import { ReactIcon } from '@/components/icons/react-icon'
+import { NextJSSquareIcon } from '@/components/icons/nextjs-square-icon'
+
 import { Separator } from '@/components/ui/separator'
 import { Hero } from '@/components/content/hero'
 
@@ -17,6 +20,29 @@ export default function ExperiencePage() {
           description='A look at my professional work experience.'
         />
         <Separator />
+        <p>
+          I love crafting interfaces and products with{' '}
+          <a
+            href='https://react.dev/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='inline-flex items-center rounded border border-neutral-200 bg-neutral-50 px-1.5 py-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100'
+          >
+            <ReactIcon className='mr-1 inline-flex h-3 w-3' />
+            React
+          </a>{' '}
+          and{' '}
+          <a
+            href='https://nextjs.org/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='inline-flex items-center rounded border border-neutral-200 bg-neutral-50 px-1.5 py-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100'
+          >
+            <NextJSSquareIcon className='mr-1 inline-flex h-3 w-3' />
+            Next.js
+          </a>
+          . Here&apos;s a look at my professional work experience.
+        </p>
       </div>
     </div>
   )
