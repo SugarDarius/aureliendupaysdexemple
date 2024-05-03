22f2776e99ffcec56cf7db8a805b7a941e76381f
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-03 20:25:30 +0200

    refactor: update mobile responsiveness for work card

--- components/grids/work-card.tsx
+++ components/grids/work-card.tsx
@@ -38,27 +38,27 @@ export function WorkCard({
 
   return (
     <BentoCard>
-      <div className='flex h-full flex-col justify-between gap-6 p-6 max-sm:gap-4 max-sm:p-4'>
-        <div className='flex flex-row items-center justify-between max-sm:flex-col max-sm:items-start max-sm:justify-start max-sm:gap-2'>
-          <div className='flex flex-row items-center gap-2 max-sm:flex-col max-sm:items-start max-sm:gap-0'>
+      <div className='flex h-full flex-col justify-between gap-6 p-6 max-md:gap-4 max-md:p-4'>
+        <div className='flex flex-row items-center justify-between max-md:flex-col max-md:items-start max-md:justify-start max-md:gap-2'>
+          <div className='flex flex-row items-center gap-2 max-md:flex-col max-md:items-start max-md:gap-0'>
             <div className='flex flex-row items-center gap-2'>
               {companyLogo ? companyLogo : null}
               <AnchorComp {...anchorProps}>
-                <h1 className='text-xl font-extrabold tracking-tighter max-sm:text-lg'>
+                <h1 className='text-xl font-extrabold tracking-tighter max-md:text-lg'>
                   {companyName}
                 </h1>
               </AnchorComp>
             </div>
-            <WorkCardSeparator className='max-sm:hidden' />
-            <span className='font-semibold text-muted-foreground max-sm:text-sm'>
+            <WorkCardSeparator className='max-md:hidden' />
+            <span className='font-semibold text-muted-foreground max-md:text-sm'>
               {jobTitle}
             </span>
           </div>
-          <div className='flex flex-row items-center gap-2 rounded-full border border-neutral-200 bg-stone-50 px-2.5 py-1 font-mono text-sm text-neutral-900 no-underline dark:border-neutral-700 dark:bg-stone-800 dark:text-neutral-100 max-sm:gap-1 max-sm:text-xs'>
+          <div className='flex flex-row items-center gap-2 rounded-full border border-neutral-200 bg-stone-50 px-2.5 py-1 font-mono text-sm text-neutral-900 no-underline dark:border-neutral-700 dark:bg-stone-800 dark:text-neutral-100 max-md:gap-1 max-md:text-xs'>
             {startDate}
             {endDate ? (
               <>
-                <WorkCardSeparator className='w-2 max-sm:w-1' />
+                <WorkCardSeparator className='w-2 max-md:w-1' />
                 {endDate}
               </>
             ) : null}
