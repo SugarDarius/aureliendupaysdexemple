2782497dc6855e57d80f5b5efd334bcda1bb6667
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-03 20:22:16 +0200

    refactor: udpate mobile responsiveness for twitter bento item

--- components/bento/twitter-bento-item.tsx
+++ components/bento/twitter-bento-item.tsx
@@ -17,7 +17,7 @@ export function TwitterBentoItem() {
         rel='noopener noreferrer'
       >
         <div className='relative flex aspect-square flex-col'>
-          <div className='flex size-full flex-col gap-1 p-4 max-sm:p-3'>
+          <div className='flex size-full flex-col gap-1 p-4 max-md:p-3'>
             <div className='flex flex-none flex-row items-center justify-between'>
               <div className='gap flex flex-row items-center gap-1.5'>
                 <div className='flex h-auto w-auto flex-col max-sm:hidden'>
@@ -43,12 +43,12 @@ export function TwitterBentoItem() {
             <div className='flex w-full flex-auto flex-col gap-4'>
               <TwitterPostWidget />
             </div>
-            <div className='relative flex w-full flex-row items-center justify-center gap-2 rounded-full border border-neutral-200 bg-neutral-50 p-1.5 text-neutral-900 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 max-sm:p-1 max-sm:text-[10px]'>
+            <div className='relative flex w-full flex-row items-center justify-center gap-2 rounded-full border border-neutral-200 bg-neutral-50 p-1.5 text-neutral-900 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 max-md:p-1 max-md:text-[10px]'>
               <span className='text-xs font-semibold'>See my account</span>
               <PaperAirplaneIcon className='size-3' />
               <VFXBorderBeam
                 /* eslint-disable-next-line prettier/prettier */
-                className='vfx-border-beam-size-[52px] max-sm:vfx-border-beam-size-10 vfx-border-beam-duration-[6s] vfx-border-beam-color-from-violet-400 vfx-border-beam-color-to-fuchsia-700 dark:vfx-border-beam-color-from-cyan-300 dark:vfx-border-beam-color-to-sky-600'
+                className='vfx-border-beam-size-[52px] max-md:vfx-border-beam-size-10 vfx-border-beam-duration-[6s] vfx-border-beam-color-from-violet-400 vfx-border-beam-color-to-fuchsia-700 dark:vfx-border-beam-color-from-cyan-300 dark:vfx-border-beam-color-to-sky-600'
               />
             </div>
           </div>
