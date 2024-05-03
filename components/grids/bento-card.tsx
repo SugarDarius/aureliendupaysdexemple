f7e877ed8910519e33456b059fb7ee1bb0be02c4
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-03 17:04:59 +0200

    chore: create vfx background dot component

--- components/grids/bento-card.tsx
+++ components/grids/bento-card.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { cva, type VariantProps } from 'class-variance-authority'
+
 import { cn } from '@/lib/utils'
+import { VFXBackgroundDot } from '@/components/ui-vfx/vfx-background-dot'
 
 const bentoCardVariants = cva(
   'relative flex flex-col overflow-hidden rounded-2xl border border-neutral-200 bg-stone-50 dark:border-neutral-800 dark:bg-stone-900',
@@ -27,12 +29,7 @@ export function BentoCard({
   return (
     <div className={cn(bentoCardVariants({ variant, className }))}>
       <div className='relative flex h-full w-full flex-col overflow-hidden'>
-        <div
-          /* eslint-disable-next-line prettier/prettier */
-          className='absolute bottom-0 left-0 right-0 top-0 m-auto h-[86%] w-[86%] bg-dot-black/[0.2] dark:bg-dot-white/[0.2]'
-        >
-          <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-stone-50 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-stone-900'></div>
-        </div>
+        <VFXBackgroundDot />
         <div className='relative z-[2] flex h-full w-full flex-col'>
           {children}
         </div>
---
+++ components/ui-vfx/vfx-background-dot.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+
+export function VFXBackgroundDot({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        /* eslint-disable-next-line prettier/prettier */
+        'absolute bottom-0 left-0 right-0 top-0 m-auto h-[86%] w-[86%] bg-dot-black/[0.2] dark:bg-dot-white/[0.2]',
+        className
+      )}
+    >
+      <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-stone-50 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-stone-900'></div>
+    </div>
+  )
+}
