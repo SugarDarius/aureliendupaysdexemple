b62fd5278be2058f740cdae02c564e51e7797659
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-04 10:00:43 +0200

    refactor: simplify css classes

--- components/ui-vfx/vfx-border-beam.tsx
+++ components/ui-vfx/vfx-border-beam.tsx
@@ -13,7 +13,7 @@ export function VFXBorderBeam({ className }: { className?: string }) {
         /* eslint-disable-next-line prettier/prettier */
         '![mask-clip:padding-box,border-box] ![mask-composite:intersect] [mask:linear-gradient(transparent,transparent),linear-gradient(white,white)]',
         /* eslint-disable-next-line prettier/prettier */
-        'after:animate-vfx-border-beam after:absolute after:aspect-square after:w-[calc(var(--tw-vfx-border-beam-size))] after:[animation-delay:var(--tw-vfx-border-beam-delay)] after:[background:linear-gradient(to_left,var(--tw-vfx-border-beam-color-from),var(--tw-vfx-border-beam-color-to),transparent)] after:[offset-anchor:calc(var(--tw-vfx-border-beam-anchor)*1%)_50%] after:[offset-path:rect(0_auto_auto_0_round_calc(var(--tw-vfx-border-beam-size)))]',
+        'after:animate-vfx-border-beam after:absolute after:aspect-square after:w-[--tw-vfx-border-beam-size] after:[animation-delay:var(--tw-vfx-border-beam-delay)] after:[background:linear-gradient(to_left,var(--tw-vfx-border-beam-color-from),var(--tw-vfx-border-beam-color-to),transparent)] after:[offset-anchor:calc(var(--tw-vfx-border-beam-anchor)*1%)_50%] after:[offset-path:rect(0_auto_auto_0_round_var(--tw-vfx-border-beam-size))]',
         className
       )}
     />
