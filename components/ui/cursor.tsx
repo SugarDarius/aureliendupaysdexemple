91fea4297df088d4048e434e5c91423ff4ee8fe0
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-06 11:52:43 +0200

    refactor: add cursor component

---
+++ components/ui/cursor.tsx
@@ -0,0 +1,15 @@
+export function Cursor(props: React.HTMLAttributes<SVGElement>) {
+  return (
+    <svg
+      {...props}
+      width='32'
+      height='44'
+      viewBox='0 0 24 36'
+      fill='currentColor'
+      stroke='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path d='M5.65376 12.3673H5.46026L5.31717 12.4976L0.500002 16.8829L0.500002 1.19841L11.7841 12.3673H5.65376Z' />
+    </svg>
+  )
+}
