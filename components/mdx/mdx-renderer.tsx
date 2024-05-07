627adb928ae3a26ba4b5ec9cc690bc88e86846be
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-07 17:42:51 +0200

    refactor: create mdx renderer components

--- components/mdx/mdx-renderer.tsx
+++ components/mdx/mdx-renderer.tsx
@@ -1,5 +1,21 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
+import { cn } from '@/lib/utils'
+
+const HeadingOne = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) => (
+  <h1
+    className={cn('text-4xl font-bold tracking-tighter', className)}
+    {...props}
+  />
+)
+
+const components = {
+  h1: HeadingOne,
+}
+
 export function MDXRenderer({ source }: { source: string }) {
-  return <MDXRemote source={source} />
+  return <MDXRemote source={source} components={components} />
 }
