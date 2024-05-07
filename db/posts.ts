8437e80d8b744381c073f7fde60bb0355aa85cc3
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-07 16:47:45 +0200

    chore: create posts db

---
+++ db/posts.ts
@@ -0,0 +1,62 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+import { compileMDX } from 'next-mdx-remote/rsc'
+import { slugify } from '@/lib/utils'
+
+const POST_EXT_NAME = '.mdx'
+function getMDXFiles(dirName: string): string[] {
+  const dir = fs.readdirSync(dirName)
+  const files = dir.filter(
+    (file): boolean => path.extname(file) === POST_EXT_NAME
+  )
+
+  return files
+}
+
+type PostMetadata = {
+  title: string
+  publishedAt: string
+  summary: string
+  image?: string
+}
+
+type PostMDXData = { metadata: PostMetadata; content: React.ReactElement }
+
+async function readMDXFile(filePath: string): Promise<PostMDXData> {
+  const rawContent = fs.readFileSync(filePath, 'utf-8')
+  const post = await compileMDX<PostMetadata>({
+    source: rawContent,
+    options: { parseFrontmatter: true },
+  })
+
+  return { metadata: post.frontmatter, content: post.content }
+}
+
+type Post = PostMDXData & {
+  slug: string
+}
+
+async function getMDXData(dirName: string): Promise<Post[]> {
+  const files = getMDXFiles(dirName)
+  const posts: Post[] = []
+
+  for (const file of files) {
+    const postMDXData = await readMDXFile(path.join(dirName, file))
+    const slug = slugify(path.basename(file, path.extname(file)))
+
+    posts.push({
+      slug,
+      ...postMDXData,
+    })
+  }
+
+  return posts
+}
+
+export async function getPosts(src: 'blog' | 'craft'): Promise<Post[]> {
+  const dirName = path.join(process.cwd(), 'content/' + src)
+  const posts = await getMDXData(dirName)
+
+  return posts
+}
