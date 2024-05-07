a36960aa9a4c0158961a3b0bcf96cc7db017c1eb
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-07 14:50:13 +0200

    refactor: use directly exposed env var

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,8 @@ import { MetadataRoute } from 'next'
 import { env } from '@/config/env'
 import { navigationItems } from '@/lib/navigation'
 
-export const baseUrl = env.NEXT_PUBLIC_PRODUCTION_URL
+export const baseUrl =
+  'https://' + env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const routes = navigationItems.map(({ href }) => ({
--- config/env.ts
+++ config/env.ts
@@ -3,9 +3,10 @@ import { z } from 'zod'
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_PRODUCTION_URL: z.string().url(),
+    NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: z.string().url(),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_PRODUCTION_URL: process.env.NEXT_PUBLIC_PRODUCTION_URL,
+    NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL:
+      process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
   },
 })
