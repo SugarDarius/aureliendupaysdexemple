49866b048e97444f4655623b59ed00aaf2a32bbc
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-10 08:45:30 +0200

    refactor: re-up slugify util

--- lib/utils.ts
+++ lib/utils.ts
@@ -15,3 +15,14 @@ export function pick<T>(collection: T[]): T {
 
   return collection[index]
 }
+
+export function slugify(input: string): string {
+  return input
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w-]+/g, '') // Remove all non-word characters except for -
+    .replace(/--+/g, '-') // Replace multiple - with single -
+}
