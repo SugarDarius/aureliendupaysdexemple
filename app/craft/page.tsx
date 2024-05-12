31bcb21ae328c60573ebf8923c9971d45119c31b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-12 14:39:28 +0200

    refactor: scaffold craft page

--- app/craft/page.tsx
+++ app/craft/page.tsx
@@ -1,15 +1,27 @@
 import type { Metadata } from 'next'
-import { ComingSoonPageContent } from '@/components/content/coming-soon-page-content'
+
+// import { getMDXPages } from '@/db/mdx-content'
+import { PageHero } from '@/components/content/page-hero'
+import { Separator } from '@/components/ui/separator'
 
 export const metadata: Metadata = {
   title: 'Craft',
   description: 'Check my personal work I craft',
 }
 
-export default function CraftPage() {
+export default async function CraftPage() {
+  // const pages = await getMDXPages('craft')
+
   return (
-    <div className='relative flex h-full w-full flex-col'>
-      <ComingSoonPageContent />
+    <div className='relative flex h-full w-full flex-col items-center'>
+      <div className='flex h-full w-full max-w-4xl flex-col gap-10 px-12 py-24 max-sm:px-4 min-[1025px]:px-0'>
+        <PageHero
+          title='Craft'
+          description='A look at my craft and contributions'
+        />
+        <Separator />
+        <div className='flex h-auto w-full flex-col'></div>
+      </div>
     </div>
   )
 }
