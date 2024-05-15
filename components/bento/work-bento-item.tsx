2bb70532a44d9bcbd5b44be37f8461b9a9f8dfe8
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-15 13:25:15 +0200

    refactor: add work preview image

--- components/bento/work-bento-item.tsx
+++ components/bento/work-bento-item.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { PageBentoItem } from '@/components/bento/page-bento-item'
 
 export function WorkBentoItem() {
@@ -7,7 +8,18 @@ export function WorkBentoItem() {
       description='Check my work and contributions'
       href='/work'
     >
-      <div className='flex h-full w-full flex-col items-center'></div>
+      <div className='flex h-full w-full flex-col items-center justify-end'>
+        <div className='relative flex size-4/5 flex-col'>
+          <Image
+            src='/medias/images/work-preview@4.png'
+            fill
+            alt='tech stack preview'
+            style={{ objectFit: 'contain' }}
+            sizes='1024px'
+            priority
+          />
+        </div>
+      </div>
     </PageBentoItem>
   )
 }
