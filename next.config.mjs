7c46bc37f3f9a43c76db32cf72cdd829d1148c6b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-20 11:17:57 +0200

    refactor: add logging options for external fetcheds

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['next-mdx-remote'],
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 }
 
 export default nextConfig
