a16c7b1eaaab7e4f8a50d9b06e99ed99b1fe03e2
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-20 19:06:32 +0200

    refactor: scaffold weather widget

--- components/bento/stand-by-bento-item.tsx
+++ components/bento/stand-by-bento-item.tsx
@@ -1,12 +1,16 @@
 import { BentoCard } from '@/components/grids/bento-card'
 import { ClockWidget } from '@/components/widgets/clock-widget'
+import { WeatherWidget } from '@/components/widgets/weather-widget'
 
 export function StandByBentoItem() {
   return (
     <BentoCard className='h-full'>
-      <div className='relative flex h-full w-full flex-col'>
+      <div className='relative flex h-full w-full flex-row items-center'>
         <div className='flex h-full w-1/2 flex-col'>
           <ClockWidget />
+        </div>
+        <div className='flex h-full w-1/2 flex-col'>
+          <WeatherWidget />
         </div>
       </div>
     </BentoCard>
---
+++ components/widgets/weather-widget.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export function WeatherWidget() {
+  return (
+    <div className='flex h-full w-full flex-col items-center justify-center p-4'></div>
+  )
+}
