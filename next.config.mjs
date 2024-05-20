930b9836bfb74ce6c66bd3539f995323a0faa9fd
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-20 12:03:33 +0200

    refactor: add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,18 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' va.vercel-scripts.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  media-src 'none';
+  font-src 'self' data:;
+  object-src 'none';
+  connect-src *;
+  frame-src 'self' data:;
+  frame-ancestors 'none';
+  form-action 'self';
+  upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['next-mdx-remote'],
@@ -5,6 +20,44 @@ const nextConfig = {
     fetches: {
       fullUrl: true,
     },
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\n/g, ''),
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            // reduce to one year
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+        ],
+      },
+    ]
   },
 }
 
