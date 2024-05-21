c86838a24e64320678be6f63c9dc2ab6391f024e
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-21 11:15:44 +0200

    refactor: update text size

--- components/widgets/clock-widget.tsx
+++ components/widgets/clock-widget.tsx
@@ -9,14 +9,16 @@ export function ClockWidget() {
   const [time, meridiem] = useClock()
   return (
     <div className='flex h-full w-full flex-col items-center justify-center p-4'>
-      <div className='flex flex-col'>
+      <div className='flex w-full flex-col'>
         <div className='flex w-full flex-row items-center justify-between px-1'>
           <span className='text-sm text-muted-foreground'>{meridiem}</span>
           <span className='text-sm text-muted-foreground'>
             {timezoneOffset}
           </span>
         </div>
-        <span className='font-mono text-6xl font-medium'>{time}</span>
+        <span className='font-mono text-6xl font-medium max-lg:text-[46px]'>
+          {time}
+        </span>
       </div>
     </div>
   )
