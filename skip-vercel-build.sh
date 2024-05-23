d474de4a69963594a291269a427fb447ca7ad37b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-05-23 21:20:10 +0200

    refactor: update skip build script

--- skip-vercel-build.sh
+++ skip-vercel-build.sh
@@ -4,9 +4,9 @@ echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
 
 if echo "$VERCEL_GIT_COMMIT_MESSAGE" | grep -q "\[skip build\]" ; then
     echo "🔺 - Build skipped"
-    exit 1
+    exit 0
 
 else
     echo "✅ - Build can procceed"
-    exit 0
+    exit 1
 fi
