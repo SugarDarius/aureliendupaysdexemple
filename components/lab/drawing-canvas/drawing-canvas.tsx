cda8c4a0628d682b92b946f9b923818aa9c2d4dc
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-06-02 14:39:05 +0200

    refactor: add default props for sketch canvas

--- components/lab/drawing-canvas/drawing-canvas.tsx
+++ components/lab/drawing-canvas/drawing-canvas.tsx
@@ -6,15 +6,36 @@ import {
   type ReactSketchCanvasRef as SketchCanvasRef,
 } from 'react-sketch-canvas'
 
-export function DrawingCanvas() {
+import { cn } from '@/lib/utils'
+import clsx from 'clsx'
+
+const initialStyle: React.CSSProperties = {
+  border: 'none',
+  borderRadius: 'none',
+}
+
+export function DrawingCanvas({ className }: { className?: string }) {
   const canvasRef = useRef<SketchCanvasRef>(null)
+
+  const [isLocked] = useState<boolean>(true)
   const [strokeWidth] = useState<number>(10)
 
   return (
-    <div className='relative flex size-full flex-col'>
+    <div
+      className={cn(
+        'relative flex size-full flex-col',
+        className,
+        clsx({
+          '!pointer-events-none': isLocked,
+          'pointer-events-auto': !isLocked,
+        })
+      )}
+    >
       <SketchCanvas
         ref={canvasRef}
         canvasColor='transparent'
+        style={initialStyle}
+        className='absolute left-0 top-0'
         width='100%'
         height='100%'
         strokeWidth={strokeWidth}
