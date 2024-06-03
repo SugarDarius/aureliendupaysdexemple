11c724ddc7731c0d0ac37495d12281ac926cba93
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-06-03 11:28:28 +0200

    refactor: create draw button

--- components/lab/drawing-canvas/drawing-canvas.tsx
+++ components/lab/drawing-canvas/drawing-canvas.tsx
@@ -40,6 +40,26 @@ const ColorButton = ({
   )
 }
 
+const DrawButton = ({
+  active = false,
+  onClick,
+}: {
+  active?: boolean
+  onClick: () => void
+}) => {
+  return (
+    <Button
+      size='icon'
+      variant='secondary'
+      className='size-auto rounded-full p-1.5 data-[active=true]:bg-muted-foreground data-[active=true]:text-background'
+      data-active={active}
+      onClick={onClick}
+    >
+      <PencilIcon className='size-4 stroke-[1.5px]' />
+    </Button>
+  )
+}
+
 const initialStyle: React.CSSProperties = {
   border: 'none',
   borderRadius: 'none',
@@ -57,7 +77,7 @@ export function DrawingCanvas({ className }: { className?: string }) {
     setStrokeColor(color)
   })
 
-  const handlePenColorButton = useEvent((): void => {
+  const handleDrawButtonClick = useEvent((): void => {
     setIsLocked(!isLocked)
   })
 
@@ -89,15 +109,7 @@ export function DrawingCanvas({ className }: { className?: string }) {
       </div>
       <div className='absolute bottom-0 right-4 top-0 my-auto flex flex-col items-center justify-center'>
         <div className='flex flex-col gap-1.5 rounded-full border p-2'>
-          <Button
-            size='icon'
-            variant='secondary'
-            className='size-auto rounded-full p-1.5 data-[active=true]:bg-muted-foreground data-[active=true]:text-background'
-            data-active={!isLocked}
-            onClick={handlePenColorButton}
-          >
-            <PencilIcon className='size-4 stroke-[1.5px]' />
-          </Button>
+          <DrawButton active={!isLocked} onClick={handleDrawButtonClick} />
           <ColorButton
             active={strokeColor === '#FF676D'}
             color='#FF676D'
