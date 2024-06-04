6f7fff3ddc6a6637202ecef0140af99240770bf7
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-06-04 14:25:20 +0200

    refactor: scaffold liveblocks drawing on screen page content

--- app/lab/liveblocks-drawing-on-screen/page.tsx
+++ app/lab/liveblocks-drawing-on-screen/page.tsx
@@ -1,3 +1,4 @@
+import { SparklesIcon } from '@/components/icons/sparkles-icon'
 import {
   LabPage,
   LabPageContent,
@@ -9,12 +10,19 @@ export default function LiveblocksDrawingOnScreenPage() {
   return (
     <LabPage>
       <LabPageContent>
-        <LabPageHeader>
+        <LabPageHeader className='flex-none'>
           <PageHero
             title='Liveblocks drawing on screen'
-            description='A lab to draw on screen using liveblocks.io'
+            description='A lab experiment to draw on screen using liveblocks.io'
           />
         </LabPageHeader>
+        <div className='flex w-full flex-auto flex-col'></div>
+        <div className='flex w-full flex-none flex-row items-center justify-center gap-2'>
+          <SparklesIcon className='size-4 stroke-[1.5px]' />
+          <span className='text-sm'>
+            Open this url in another window to see the magic
+          </span>
+        </div>
       </LabPageContent>
     </LabPage>
   )
--- components/content/lab-page.tsx
+++ components/content/lab-page.tsx
@@ -24,7 +24,7 @@ function Content({
   return (
     <div
       className={cn(
-        'flex h-auto min-h-full w-full flex-col items-center gap-4 px-12 pb-24 pt-12 max-sm:px-4 min-[1025px]:px-0',
+        'flex h-auto min-h-full w-full flex-col items-center gap-6 px-8 pb-24 pt-12 max-sm:px-4',
         className
       )}
     >
