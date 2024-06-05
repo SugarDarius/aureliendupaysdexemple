a8a673810348fc754dd6e9e0727359c975f4de0c
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-06-05 11:15:13 +0200

    refactor: add image placeholder

--- components/lab/video-call-frame.tsx
+++ components/lab/video-call-frame.tsx
@@ -1,6 +1,8 @@
 import { cn } from '@/lib/utils'
+import { ImagePlaceholder } from '@/components/ui-helpers/image-placeholder'
 export function VideoCallFrame({
   className,
+  children,
 }: {
   className?: string
   children?: React.ReactNode
@@ -8,9 +10,16 @@ export function VideoCallFrame({
   return (
     <div
       className={cn(
-        'relative flex min-h-full w-full flex-col bg-gradient-to-r from-violet-200 to-pink-200 p-2',
+        'relative flex h-full w-full flex-col gap-6 bg-gradient-to-r from-violet-200 to-pink-200 p-4',
         className
       )}
-    ></div>
+    >
+      <div className='flex w-full flex-auto flex-col items-center justify-center'>
+        <div className='flex aspect-video w-full flex-col overflow-hidden rounded-md bg-stone-700'>
+          {children ? children : <ImagePlaceholder className='size-full' />}
+        </div>
+      </div>
+      <div className='flex w-full flex-none flex-col'></div>
+    </div>
   )
 }
