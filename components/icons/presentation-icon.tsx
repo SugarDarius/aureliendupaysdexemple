ee86fe346ce77b18cbf06a888e820835296abf9b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-06-05 15:48:25 +0200

    refactor: add controls

---
+++ components/icons/presentation-icon.tsx
@@ -0,0 +1,18 @@
+export const PresentationIcon = (props: React.HTMLAttributes<SVGElement>) => (
+  <svg
+    {...props}
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path d='M2 3h20' />
+    <path d='M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3' />
+    <path d='m7 21 5-5 5 5' />
+  </svg>
+)
--- components/lab/video-call-frame.tsx
+++ components/lab/video-call-frame.tsx
@@ -1,7 +1,17 @@
 import Image from 'next/image'
+import {
+  Cog6ToothIcon,
+  FaceSmileIcon,
+  MicrophoneIcon,
+  VideoCameraIcon,
+} from '@heroicons/react/24/outline'
 
 import { cn } from '@/lib/utils'
+
+import { Button } from '@/components/ui/button'
 import { ImagePlaceholder } from '@/components/ui-helpers/image-placeholder'
+
+import { PresentationIcon } from '@/components/icons/presentation-icon'
 
 const Participant = ({
   className,
@@ -22,6 +32,32 @@ const Participant = ({
   </div>
 )
 
+export const ControlButton = ({
+  className,
+  active = false,
+  children,
+  onClick,
+}: {
+  className?: string
+  active?: boolean
+  children: React.ReactNode
+  onClick?: () => void
+}) => (
+  <Button
+    className={cn(
+      'size-[36px] border border-neutral-200 bg-neutral-50 p-0 text-neutral-900 transition-colors ease-linear',
+      'hover:border-neutral-700 hover:bg-neutral-800 hover:text-neutral-100 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100 dark:hover:border-neutral-200 dark:hover:bg-neutral-50 dark:hover:text-neutral-900',
+      'data-[active=true]:border-neutral-700 data-[active=true]:bg-neutral-800 data-[active=true]:text-neutral-100',
+      'dark:data-[active=true]:border-neutral-200 dark:data-[active=true]:bg-neutral-50 dark:data-[active=true]:text-neutral-900',
+      className
+    )}
+    data-active={active}
+    onClick={onClick}
+  >
+    {children}
+  </Button>
+)
+
 export function VideoCallFrame({
   className,
   shareScreenView,
@@ -34,12 +70,12 @@ export function VideoCallFrame({
   return (
     <div
       className={cn(
-        'relative flex h-full max-h-full w-full flex-col gap-6 bg-gradient-to-l from-gray-200 via-fuchsia-200 to-stone-100 p-4',
+        'relative flex h-full max-h-full w-full flex-col gap-4 bg-gradient-to-l from-gray-200 via-fuchsia-200 to-stone-100 p-4',
         className
       )}
     >
       <div className='relative flex w-full flex-auto flex-row gap-4'>
-        <div className='relative flex h-full flex-auto flex-col items-center justify-center overflow-hidden rounded-md bg-neutral-800/35'>
+        <div className='relative flex h-full flex-auto flex-col items-center justify-center overflow-hidden rounded-md bg-neutral-800/20'>
           <div className='flex aspect-video h-auto w-full flex-col items-center justify-center'>
             {shareScreenView ? (
               shareScreenView
@@ -88,7 +124,23 @@ export function VideoCallFrame({
         </div>
       </div>
       <div className='flex w-full flex-none flex-col gap-4'>
-        <div className='flex w-full flex-row items-center justify-center gap-6'>
+        <div className='flex w-full flex-row items-center justify-center gap-2'>
+          <ControlButton>
+            <MicrophoneIcon className='size-5' />
+          </ControlButton>
+          <ControlButton>
+            <VideoCameraIcon className='size-5' />
+          </ControlButton>
+          <ControlButton active>
+            <PresentationIcon className='size-5 stroke-[1.5px]' />
+          </ControlButton>
+          <ControlButton>
+            <FaceSmileIcon className='size-5' />
+          </ControlButton>
+          <ControlButton>
+            <Cog6ToothIcon className='size-5' />
+          </ControlButton>
+          <Button variant='destructive'>Leave</Button>
           {children}
         </div>
       </div>
