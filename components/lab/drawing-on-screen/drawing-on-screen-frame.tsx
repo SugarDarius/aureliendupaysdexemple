96f1d2ac7f2b2dcb48e1e25438ea9e00adc2c21f
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-08-03 21:16:28 +0200

    refactor: remove useless component

--- components/lab/drawing-on-screen/drawing-on-screen-frame.tsx
+++ components/lab/drawing-on-screen/drawing-on-screen-frame.tsx
@@ -1,10 +1,32 @@
-import { LiveblocksRoomProvider } from '@/components/lab/drawing-on-screen/liveblocks-room-provider'
+'use client'
+import { ClientSideSuspense } from '@liveblocks/react/suspense'
+
+import { labsConfig } from '@/config/labs-config'
+import {
+  LiveblocksProvider,
+  RoomProvider,
+} from '@/components/lab/drawing-on-screen/liveblocks.config'
 import { DrawingOnScreenEditor } from '@/components/lab//drawing-on-screen/drawing-on-screen-editor'
 
+const useRoomId = (): string => {
+  return labsConfig.liveblocksDrawingOnScreen.roomId
+}
+
+const SuspenseFallback = () => (
+  <div className='flex h-full w-full flex-col items-center justify-center'>
+    <div className='h-6 w-6 animate-spin rounded-full border-b-2 border-current' />
+  </div>
+)
+
 export function DrawingOnScreenFrame() {
+  const roomId = useRoomId()
   return (
-    <LiveblocksRoomProvider>
-      <DrawingOnScreenEditor />
-    </LiveblocksRoomProvider>
+    <LiveblocksProvider>
+      <RoomProvider id={roomId} initialPresence={{ isDrawing: false }}>
+        <ClientSideSuspense fallback={<SuspenseFallback />}>
+          <DrawingOnScreenEditor />
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
   )
 }
