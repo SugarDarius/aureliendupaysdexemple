cfae32866cbf3a2c7a49e347d52d9d4fded514ba
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-11-04 17:13:57 +0100

    rewrite props type definition

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -1,7 +1,10 @@
 'use-client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+
+// Note: Following https://github.com/pacocoursey/next-themes/issues/322
+// Props to be updated if they are exported again
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
